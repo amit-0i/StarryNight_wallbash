33fdc6c49f5e2e1963b0b39ec0abce0e3cb3efba
amit-0i <6329amit.cs@gmail.com>
2025-08-20 22:50:37 +0530

    chore: update script to latest HyDE changes

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 cp -r ./StarryNight_wallbash ~/.config/spicetify/Themes/StarryNight_wallbash
+mv  ~/.config/hyde/wallbash/always/spotify.dcol ~/.config/hyde/wallbash/always/spotify.dcol.bak
+mv ~/.config/hyde/wallbash/scripts/spotify.sh ~/.config/hyde/wallbash/scripts/spotify.sh.bak
 cp ./spotify.dcol ~/.config/hyde/wallbash/always/
 cp ./spotify.sh ~/.config/hyde/wallbash/scripts/
+chmod +x ~/.config/hyde/wallbash/scripts/spotify.sh
 
 config_file="$HOME/.config/spicetify/config-xpui.ini"
 sed -i '/^current_theme[[:space:]]*=/ s/=.*/= StarryNight_wallbash/' ~/.config/spicetify/config-xpui.ini
